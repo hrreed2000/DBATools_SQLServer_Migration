9f287d92dd8615a1c779685c5e0fe7900f5942d3
hrreed2000 <58342822+hrreed2000@users.noreply.github.com>
2022-10-11 04:36:01 -0500

    Add files via upload

---
+++ DBAMigrationDemo.ps1
@@ -0,0 +1,23 @@
+﻿CLS
+Find-DBAInstance -Computername localhost
+Install-dbainstance -version 2019 -sqlinstance harv2020\TheWaiter -feature Engine -Path E:\sql2019 -authenticationmode mixed
+CLS
+Copy-DBASPConfigure -Source Harv2020 -Destination HARV2020\JOEBATTERS  -whatif
+$DBAConfigureSplat = @{
+Source = 'Harv2020'
+Destination = 'HARV2020\JOEBATTERS'
+}
+Copy-DBASPConfigure @DBAConfigureSplat 
+Copy-DbaInstanceAudit –Source Harv2020-Destination HARV2020\JOEBATTERS  -whatif
+Copy-DbaInstanceAuditSpecification –Source Harv2020-Destination HARV2020\JOEBATTERS  -whatif
+
+Copy-DbaPolicyManagement –Source Harv2020-Destination HARV2020\JOEBATTERS  -whatif
+Copy-DbaXeSession –Source Harv2020-Destination HARV2020\JOEBATTERS  -whatif
+Copy-dbalogin –Source Harv2020-Destination HARV2020\JOEBATTERS -SyncSaName   -whatif
+rename-dbaLogin -sqlinstance HARV2020\JOEBATTERS -login sa -newlogin 'Charles Chuckie English' -Whatif
+Copy-DbaDatabase –Source Harv2020-Destination HARV2020\JOEBATTERS -AllDatabases -BackupRestore -Sharedpath 'E:\DBAToolsDemo\' ##-UseLastBackup -whatif
+Start-DbaMigration –Source Harv2020 –Destination HARV2020\THEWAITER -BackupRestore –SharedPath 'E:\DBAToolsDemo\' -whatif
+Repair-DbaDbOrphanUser -SqlInstance harv2020\joebatters -WhatIf
+get-help get-dbauserpermission -showwindow
+Get-DbaLogin -SqlInstance Harv2020\JoeBatters -Login 'Frank Pantangeline' 
+Set-DbaLogin -SqlInstance Harv2020\JoeBatters -Login 'Frank Pantangeline' -Disable -whatif
